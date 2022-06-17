2d8b423ee7fa9dea9cc25ee0dffa9c2d1ec22b26
user <user@localhost.localdomain>
2022-06-16 23:11:07 -0700

    Update delete function

--- cogs/ModTools.py
+++ cogs/ModTools.py
@@ -1,11 +1,12 @@
 import discord
 import argparse
+import asyncio
 from discord.ext import commands
 from modules import config, sounds
 from modules.helper_functions import format_markdown, checkExists, checkGroup
 from modules.database import GetDB
 # import for buttons and dropdown UI
-from discord_components import DiscordComponents, ComponentsBot, Button, Select, SelectOption
+from discord_components import DiscordComponents, ComponentsBot, Button, Select, SelectOption, ButtonStyle
 
 class ModTools(commands.Cog):
     def __init__(self, bot):
@@ -89,6 +90,7 @@ class ModTools(commands.Cog):
         #       - Cleanup residual folders if they are empty after sound deletion 
         #       - Remove existing soundIDs from other tables (quicksounds, entry cache, entry sound tables)
         #       - Generate a message to send to members that use this sound for the above datastructures
+        #       - Somehow catch the discord.ext.commands.errors.MissingRequiredArgument and respond w/ a helpdoc
         parser = argparse.ArgumentParser()
         parser.add_argument('sound', help="Sound name to delete", nargs='+')
         member = ctx.message.author
@@ -96,16 +98,47 @@ class ModTools(commands.Cog):
         try:
             args = parser.parse_args(command_args)
             print(args)
-            if len(args.sound) == 1:
+            argslen = len(args.sound)
+            if argslen == 1:
                 sound_id = f"{args.sound[0]}"
                 group = "root"
-            else:
+            elif argslen == 2:
                 sound_id = f"{args.sound[0]} {args.sound[1]}"
                 group = args.sound[0]
+            else:
+                await ctx.reply(format_markdown("Error: Too many arguments supplied."), delete_after=10)
+                return
             
+
             if checkExists(group, sound_id):
                 db = GetDB(config.database_path)
-                data = db.cursor.execute(f"SELECT EXISTS(SELECT sound_name FROM sounds WHERE ")
+                db.set_row_factory(lambda cursor, row: row[0:1])
+                data = db.cursor.execute(f"SELECT EXISTS(SELECT * FROM sounds WHERE author_id={member.id} AND sound_id=\"{sound_id}\")").fetchone()
+                if data[0] != 1:
+                    await ctx.reply(format_markdown(f"Error: You must be the author of the sound to delete it."), delete_after=10)
+                    return
+                try:
+                    message = await ctx.reply(
+                        f"{ctx.author.mention} you are about to delete sound `{sound_id}`.\nPlease confirm your choice or cancel.",
+                        components = [
+                            [
+                                Button(label = "Okay", custom_id = True, style = ButtonStyle.green),
+                                Button(label = "Cancel", custom_id = False, style = ButtonStyle.red)
+                            ]
+                        ]
+                    )
+                    interaction = await self.bot.wait_for("button_click", timeout = 30.0)
+                    await message.delete()
+                    print(interaction.custom_id)
+                    if interaction.custom_id:
+                        print("do database stuff")
+                    else:
+                        return
+
+                except asyncio.TimeoutError:
+                    await ctx.author.send(format_markdown(f"Timeout: Delete operation cancelled."))
+                    return
+
             else:
                 await ctx.reply(format_markdown(f"Error: {sound_id} does not exist."), delete_after=10)
 
@@ -117,6 +150,7 @@ class ModTools(commands.Cog):
         except argparse.ArgumentTypeError as e:
             print(e)
             pass
+        # Catch argparses SystemExit error 
         except SystemExit:
             pass
 
