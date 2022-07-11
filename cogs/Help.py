2a74a866cec359483646183f2a18dd2ebc2f5633
jnathan0 <joshuanathandigital@gmail.com>
2022-07-11 01:12:54 +0000

    Add more functionality to help command

--- cogs/Help.py
+++ cogs/Help.py
@@ -6,38 +6,45 @@ from discord.ext import commands
 class Help(commands.Cog):
     def __init__(self: object, bot: object):
         self.bot = bot
+        self.global_commands = self.get_global_commands()
         self.global_helpdoc = self.get_helpdoc()
+        
+    def get_global_commands(self):
+        commands_dict = {}
+        for cog in self.bot.cogs:
+            for command in self.bot.get_cog(cog).get_commands():
+                commands_dict[command.name] = command
+        return commands_dict
+        
 
     def get_helpdoc(self):
-        pass
-        # helpdoc = ""
-        # for cog in self.bot.cogs:
-        #     print(f"{cog}\n")
-        #     for command in self.bot.get_cog(cog).get_commands():
-        #         print(f"{command.all_commands.items()}\n")
-
-    @commands.command(aliases=['h'])
-    async def help(self, ctx):
-        """
-        Displays this message.
-        """
-        cogs_desc = ""
         embed = discord.Embed(title="Commands and subcommands",
                                 description=f"To get more detail on a command and its usage, use `{config.prefix}help <command>`\n(ex. `{config.prefix}help quicksounds`)",
                                 color=discord.Color.green())
-        for cog in self.bot.cogs:
-            for command in self.bot.get_cog(cog).get_commands():
-                if command.hidden:
-                    continue
-                cogs_desc += (get_command_help(command))
-                if "all_commands" in command.__dict__.keys():
-                    subcommands=""
-                    for subcommand in command.all_commands.values():
-                        subcommands += f"{subcommand.name}\n"
-                    embed.add_field(name=f"{command}:", value=subcommands, inline=True)
-                else:
-                    embed.add_field(name=command, value=command.name, inline=True)
-        await ctx.send(embed=embed)
+        for command in self.global_commands.values():
+            if command.hidden:
+                continue
+            if "all_commands" in command.__dict__.keys():
+                subcommands=""
+                for subcommand in command.all_commands.values():
+                    subcommands += f"{subcommand.name}\n"
+                embed.add_field(name=f"{command}:", value=subcommands, inline=True)
+            else:
+                embed.add_field(name=command, value=command.name, inline=True)
+        return embed
+
+    @commands.command(aliases=['h'])
+    async def help(self, ctx, *args):
+        """
+        Displays this message.
+        Use a specific command in addition to help to view help for that command.
+        e.x `help stats`
+        """
+        if not args:
+            await ctx.send(embed=self.global_helpdoc)
+        if args[0] in self.global_commands.keys():
+            command = self.global_commands[args[0]]
+            await ctx.send(embed=get_command_help(command))
             
 
 def setup(bot):
