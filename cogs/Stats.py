b838226cf810dc491057d4f4c66ce158f1e2cecd
jnathan0 <joshuanathandigital@gmail.com>
2022-07-11 01:14:06 +0000

    refactor stats command and subcomands to use commands.group

--- cogs/Stats.py
+++ cogs/Stats.py
@@ -12,10 +12,14 @@ class Stats(commands.Cog, commands.Command):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
+    @commands.group()
     @commands.guild_only()
     async def stats(self,ctx):
-        """View the play count, author, and date added of a specific sound or the amount of sounds and plays for a user"""
+        """
+        View the play count, author, and date added of a specific sound
+        or the amount of sounds and plays for a user
+        Example Usage: `stats`
+        """
         command = ctx.message.content.split(config.prefix)[1]
         if command == "stats":
             db = GetDB(config.database_path)
@@ -49,83 +53,15 @@ class Stats(commands.Cog, commands.Command):
                     i+=1
                 await ctx.reply(format_markdown(message))
                 db.close()
+                return
             
             except Exception as e:
-                await ctx.reply(format_markdown("ERROR: User not found, are you sure they have added anything to the bot?"))
-        
-        elif command.split("stats ")[1] == "new":
-            try:
-                member = ctx.message.author
-
-                mydate = date.today()
-                last_month = mydate - relativedelta(months=+1)
-                day_string = last_month.strftime("%Y-%m-%d")
-                today_string = mydate.strftime("%Y-%m-%d")
-
-
-                db = GetDB(config.database_path)
-                db.cursor.execute("SELECT sound_id, date FROM sounds WHERE \"date\" >= ? ORDER BY date ASC", [day_string])
-                info = db.cursor.fetchall()
-                await member.send(f"------ SOUNDS ADDED FROM {day_string} - {today_string} ------")
-                message = ""
-                for item in info:
-                    line = f"| Date: {item[1]} | Name: {item[0]} |\n"
-                    if (len(message)+len(line)) > 1994:
-                        await member.send(format_markdown(message))
-                        message = ""
-                        message += line
-                    else:
-                        message+=line
-                await member.send(format_markdown(message))
-                await member.send("--------------------------")
-                db.close()
-
-            except Exception as e:
-                print(e)
-                print(traceback.format_exec())
-                await ctx.send(format_markdown("ERROR: Something broke for this specific command, maybe it will get fixed."))
-                return
-
-        # This block is for checking aggregated, time-ranged stats from the Redis server #
-        # hourly, daily, weekly and monthly stats #
-        # Time is based on a rolling period from when the command is issued (i.e. 'daily' wil request stats 24 hours ago to current time) #
-        
-        elif command.split("stats ")[1] in ["hour", "day", "week", "month"]:
-            try:
-                query_range = command.split("stats ")[1]
-                member = ctx.message.author
-                redis_metadata = config.redis_metadata
-                # redis_metadata = redis.StrictRedis('localhost', 6379, db = 1, charset = "utf-8", decode_responses = True)
-
-                hours_dict = {"hour": 1, "day": 24, "week": 168, "month": 730}
-                stream_key = "plays:metadata"
-
-                start_time = datetime.datetime.now() - datetime.timedelta(hours = hours_dict[query_range])
-                start_time = int(start_time.timestamp()*1000)
-
-
-                query_result = redis_metadata.xrange(name = str(stream_key), min = start_time, max = "+")
-                if not query_result:
-                    await member.send(format_markdown("Looks like there haven't been any sounds played within the last hour, time to get shitposting."))
-                    return
-
-                dict_list = []
-                for item in query_result:
-                    dict_list.append(item[1])
-                foo = pd.DataFrame.from_dict(dict_list)
-                bar = foo["sound_id"].value_counts()
-                header = f"Sound Name          Plays\n-------------------------\n"
-                await ctx.reply(format_markdown(header + bar.head(n=10).to_string()))
-                redis_metadata.close()
-
-            except Exception as e:
-                await ctx.reply(format_markdown("Something happen"))
-                return
-
-
+                await ctx.reply(format_markdown("ERROR: User not found, are you sure they have added anything to the bot?"))    
         else:
             try:
                 sound_id = command.split("stats ")[1]
+                if sound_id.lower() in ['hour', 'day', 'week', 'month', 'new']:
+                    return
                 db = GetDB(config.database_path)
                 db.cursor.execute("SELECT * FROM sounds WHERE sound_id=\""+sound_id+"\"")
                 info = db.cursor.fetchall()
@@ -139,10 +75,204 @@ class Stats(commands.Cog, commands.Command):
                 # message = info[0][1] + "\n\nDate Created: "+info[0][5]+"\nAuthor: "+username+"\nParent folder: "+info[0][2]+"\nPlays: "+str(info[0][4])
                 await ctx.send(embed=embedvar)
                 db.close()
+                return
 
             except Exception as e:
-                await ctx.send(format_markdown("ERROR: Sound file does not exist, please try using a specific sound if you think it does."))
+                await ctx.send(format_markdown("ERROR: Sound file does not exist, please try using a specific sound if you think it does."), delete_after=5)
                 return
+
+    @stats.group()
+    @commands.guild_only()
+    async def new(self, ctx):
+        """
+        Displays all the new sounds added from one month ago until today.
+        Example Usage:
+            `stats new`
+        """
+        try:
+            member = ctx.message.author
+
+            mydate = date.today()
+            last_month = mydate - relativedelta(months=+1)
+            day_string = last_month.strftime("%Y-%m-%d")
+            today_string = mydate.strftime("%Y-%m-%d")
+
+
+            db = GetDB(config.database_path)
+            db.cursor.execute("SELECT sound_id, date FROM sounds WHERE \"date\" >= ? ORDER BY date ASC", [day_string])
+            info = db.cursor.fetchall()
+            await member.send(f"------ SOUNDS ADDED FROM {day_string} - {today_string} ------")
+            message = ""
+            for item in info:
+                line = f"| Date: {item[1]} | Name: {item[0]} |\n"
+                if (len(message)+len(line)) > 1994:
+                    await member.send(format_markdown(message))
+                    message = ""
+                    message += line
+                else:
+                    message+=line
+            await member.send(format_markdown(message))
+            await member.send("--------------------------")
+            db.close()
+
+        except Exception as e:
+            print(e)
+            print(traceback.format_exec())
+            await ctx.send(format_markdown("ERROR: Something broke for this specific command, maybe it will get fixed."))
+            return
+
+# These commands are for checking aggregated, time-ranged stats from the Redis server #
+# hourly, daily, weekly and monthly stats #
+# Time is based on a rolling period from when the command is issued (i.e. 'daily' wil request stats 24 hours ago to current time) #
+
+    @stats.group()
+    @commands.guild_only()
+    async def hour(self, ctx):
+        """
+        Check the count of the played sounds from an hour until now
+
+        Example Usage:
+            `stats hour`
+        """
+        try:
+            member = ctx.message.author
+            redis_metadata = config.redis_metadata
+            # redis_metadata = redis.StrictRedis('localhost', 6379, db = 1, charset = "utf-8", decode_responses = True)
+            stream_key = "plays:metadata"
+
+            start_time = datetime.datetime.now() - datetime.timedelta(hours = 1)
+            start_time = int(start_time.timestamp()*1000)
+
+
+            query_result = redis_metadata.xrange(name = str(stream_key), min = start_time, max = "+")
+            if not query_result:
+                await member.send(format_markdown("Looks like there haven't been any sounds played within the last hour, time to get shitposting."))
+                return
+
+            dict_list = []
+            for item in query_result:
+                dict_list.append(item[1])
+            foo = pd.DataFrame.from_dict(dict_list)
+            bar = foo["sound_id"].value_counts()
+            header = f"Sound Name          Plays\n-------------------------\n"
+            await ctx.reply(format_markdown(header + bar.head(n=10).to_string()))
+            redis_metadata.close()
+
+        except Exception as e:
+            await ctx.reply(format_markdown("Something happen"))
+            return
+
+
+    @stats.group()
+    @commands.guild_only()
+    async def day(self, ctx):
+        """
+        Check the count of the played sounds from 24 hours until now
+
+        Example Usage:
+            `stats day`
+        """
+        try:
+            member = ctx.message.author
+            redis_metadata = config.redis_metadata
+            # redis_metadata = redis.StrictRedis('localhost', 6379, db = 1, charset = "utf-8", decode_responses = True)
+            stream_key = "plays:metadata"
+
+            start_time = datetime.datetime.now() - datetime.timedelta(hours = 24)
+            start_time = int(start_time.timestamp()*1000)
+
+
+            query_result = redis_metadata.xrange(name = str(stream_key), min = start_time, max = "+")
+            if not query_result:
+                await member.send(format_markdown("Looks like there haven't been any sounds played within the day, time to get shitposting."))
+                return
+
+            dict_list = []
+            for item in query_result:
+                dict_list.append(item[1])
+            foo = pd.DataFrame.from_dict(dict_list)
+            bar = foo["sound_id"].value_counts()
+            header = f"Sound Name          Plays\n-------------------------\n"
+            await ctx.reply(format_markdown(header + bar.head(n=10).to_string()))
+            redis_metadata.close()
+
+        except Exception as e:
+            await ctx.reply(format_markdown("Something happen"))
+            return
+
+    @stats.group()
+    @commands.guild_only()
+    async def week(self, ctx):
+        """
+        Check the count of the played sounds from 7 days ago until now
+
+        Example Usage:
+            `stats week`
+        """
+        try:
+            member = ctx.message.author
+            redis_metadata = config.redis_metadata
+            # redis_metadata = redis.StrictRedis('localhost', 6379, db = 1, charset = "utf-8", decode_responses = True)
+            stream_key = "plays:metadata"
+
+            start_time = datetime.datetime.now() - datetime.timedelta(hours = 168)
+            start_time = int(start_time.timestamp()*1000)
+
+
+            query_result = redis_metadata.xrange(name = str(stream_key), min = start_time, max = "+")
+            if not query_result:
+                await member.send(format_markdown("Looks like there haven't been any sounds played within the last week, time to get shitposting."))
+                return
+
+            dict_list = []
+            for item in query_result:
+                dict_list.append(item[1])
+            foo = pd.DataFrame.from_dict(dict_list)
+            bar = foo["sound_id"].value_counts()
+            header = f"Sound Name          Plays\n-------------------------\n"
+            await ctx.reply(format_markdown(header + bar.head(n=10).to_string()))
+            redis_metadata.close()
+
+        except Exception as e:
+            await ctx.reply(format_markdown("Something happen"))
+            return
+
+    @stats.group()
+    @commands.guild_only()
+    async def month(self, ctx):
+        """
+        Check the count of the played sounds from the date from last month until now
+
+        Example Usage:
+            `stats month`
+        """
+        try:
+            member = ctx.message.author
+            redis_metadata = config.redis_metadata
+            # redis_metadata = redis.StrictRedis('localhost', 6379, db = 1, charset = "utf-8", decode_responses = True)
+            stream_key = "plays:metadata"
+
+            start_time = datetime.datetime.now() - datetime.timedelta(hours = 730)
+            start_time = int(start_time.timestamp()*1000)
+
+
+            query_result = redis_metadata.xrange(name = str(stream_key), min = start_time, max = "+")
+            if not query_result:
+                await member.send(format_markdown("Looks like there haven't been any sounds played within the last month, time to get shitposting."))
+                return
+
+            dict_list = []
+            for item in query_result:
+                dict_list.append(item[1])
+            foo = pd.DataFrame.from_dict(dict_list)
+            bar = foo["sound_id"].value_counts()
+            header = f"Sound Name          Plays\n-------------------------\n"
+            await ctx.reply(format_markdown(header + bar.head(n=10).to_string()))
+            redis_metadata.close()
+
+        except Exception as e:
+            await ctx.reply(format_markdown("Something happen"))
+            return
 
 def setup(bot):
     bot.add_cog(Stats(bot))
