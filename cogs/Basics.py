5e9c2f498ca5b975cf3e21e4d8d5a91712050ac6
user <joshuanathandigital@gmail.com>
2023-01-23 03:12:26 +0000

    refactor Basics cog to use slashcommands

--- cogs/Basics.py
+++ cogs/Basics.py
@@ -1,6 +1,5 @@
 import discord
-import time
-from datetime import datetime
+from discord import app_commands
 from discord.ext import commands
 from modules import config
 from modules.helper_functions import format_markdown, restart_bot
@@ -12,16 +11,14 @@ class Basics(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def ping(self, ctx: commands.Context):
-        """
-        Shows the Gateway Ping.
-        """
-        await ctx.typing()
-        await ctx.send(f"> **PONG**\n:hourglass: gateway ping: {round(self.bot.latency * 1000)}ms :hourglass:")
+    @app_commands.command(name="ping")
+    async def ping(self, interaction: discord.Interaction) -> None:
+        """Shows the Gateway Ping."""
+        await interaction.channel.typing()
+        await interaction.response.send_message(f"> **PONG**\n:hourglass: gateway ping: {round(self.bot.latency * 1000)}ms :hourglass:")
 
-    @commands.command()
-    async def github(self, ctx):
+    @app_commands.command(name="github")
+    async def github(self, interaction: discord.Interaction) -> None:
         """
         Gives you my source code.
         """
@@ -31,15 +28,18 @@ class Basics(commands.Cog):
         embed.set_image(
             url="https://cdn.discordapp.com/avatars/593170973193273344/0a143cd8cfa9077570ebef54f097c882.webp")
         try:
-            await ctx.send(embed=embed)
+            await interaction.response.send_message(embed=embed)
         except discord.Forbidden:  # failover on 403 while sending embed. not used in invite becasue it would look awfull
-            await ctx.send(
+            await interaction.response.send_message(
                 "Want to report bug?\nSubmit feature request?\nMake new feature?\nBot code is available on github page:\n<https://github.com/Jnathan0/doot-doot-wow>")
 
-    @commands.command()
-    @commands.guild_only()
-    @commands.has_role(config.owb_id)#decorator to see if whoever requested the command has the role specified, takes roleid argument in int or string form. 
+    @app_commands.command(name="restart")
+    @app_commands.guild_only()
+    @app_commands.checks.has_role(config.owb_id) #decorator to see if whoever requested the command has the role specified, takes roleid argument in int or string form. 
     async def restart(self, ctx):
+        """
+        Restarts the bot
+        """
         try:
             file = discord.File(str(config.gifs_path+"illbeback.gif"))
             await ctx.send(file=file)
@@ -49,7 +49,7 @@ class Basics(commands.Cog):
     
     @restart.error#error handaling for the restart function
     async def restart_error(self,ctx,error):
-        if isinstance(error, commands.MissingRole):#if @commands.has_role returns with MissingRole error, send message
+        if isinstance(error, discord.app_commands.MissingRole):#if @app_commands.checks.has_role() returns with MissingRole error, send message
             await ctx.send(format_markdown("Cannot restart, \'owb\' role required"))
 
 async def setup(bot):
