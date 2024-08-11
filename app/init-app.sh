e663567a3f829e5da54dcbb6adc5ecc3041b5526
Jnathan0 <55114731+Jnathan0@users.noreply.github.com>
2024-08-11 13:42:34 -0700

    Update project to use poetry (#24)
    * Update project to use poetry

--- app/init-app.sh
+++ app/init-app.sh
@@ -1,14 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
 set -exo pipefail
-
 
 nohup /env/bin/rqscheduler > scheduler.log &
 nohup rqworker --with-scheduler metadata --name metadata > metadata_worker.log &
 nohup rqworker --with-scheduler generic_worker --name generic_worker > generic_worker.log &
 nohup rqworker plays --name plays > plays_worker.log &
 
-source /env/bin/activate
-
 # run the app
-python3 main.py 
+poetry run python3 main.py 
